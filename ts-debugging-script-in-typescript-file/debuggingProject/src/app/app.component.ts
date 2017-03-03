9a2b7e344a5a6c0217a12a06ce082d67ebe31769
abhiit89 <abhi.aec89@gmail.com>
2017-03-03 12:26:37 +0530

    testing for inline template - possible only from undex.html

--- ts-debugging-script-in-typescript-file/debuggingProject/src/app/app.component.ts
+++ ts-debugging-script-in-typescript-file/debuggingProject/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  hitMe(){
+    window.alert("Hello World");
+  }
+
+  hitMeAgain(){
+    alert("Hello World");
+  }
+
 }
